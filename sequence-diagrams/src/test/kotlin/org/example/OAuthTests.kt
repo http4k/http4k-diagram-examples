b6f091bb4ce280e2b6f3ad1777769033b5a33479
Ivan Sanchez <ivan@gourame.com>
2022-11-13 11:22:58 +0000

    Test preparation

---
+++ sequence-diagrams/src/test/kotlin/org/example/OAuthTests.kt
@@ -0,0 +1,66 @@
+package org.example
+
+import org.http4k.core.*
+import org.http4k.core.Method.GET
+import org.http4k.core.Status.Companion.OK
+import org.http4k.events.Events
+import org.http4k.events.HttpEvent
+import org.http4k.events.then
+import org.http4k.filter.ClientFilters.RequestTracing
+import org.http4k.filter.DebuggingFilters.PrintRequestAndResponse
+import org.http4k.filter.ResponseFilters.ReportHttpTransaction
+import org.http4k.routing.bind
+import org.http4k.routing.reverseProxy
+import org.http4k.routing.routes
+import org.http4k.tracing.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+
+class OAuthTests {
+
+    // generates PlantUML diagrams in .generated/diagrams
+    @RegisterExtension
+    val events = TraceReportingEvents(AppName("oauth-examples"))
+
+    private val relyingParty = AppIncomingHttp()
+        .then(routes(
+            "/ping" bind GET to {
+                Response(OK).body("pong")
+            }
+        ))
+
+    private val authServer = AppIncomingHttp()
+        .then(routes(
+            "/indirect-ping" bind GET to {
+                Response(OK).body("pong")
+            }
+        ))
+
+    private val browser =
+        AppIncomingHttp()
+            .then(
+                reverseProxy(
+                    "relying-party" to proxiedOutbound(events, relyingParty, "relying-party"),
+                    "auth-server" to proxiedOutbound(events, authServer, "auth-server")
+                )
+            )
+
+
+    private val user = UserActor(events, browser)
+
+    @Test
+    fun `Authorization code oauth2 flow`() {
+        user(Request(GET, "https://browser/ping").header("host", "relying-party"))
+        println(events.joinToString("\n"))
+    }
+}
+
+fun UserActor(evens: Events, browser: HttpHandler) = TracedActorHttp("John Doe", browser, evens)
+
+fun proxiedOutbound(events: Events, client: HttpHandler, name: String) =
+    Filter.NoOp
+        .then(Filter { next -> { next(it.uri(it.uri.host(name))) } })
+        .then(ReportHttpTransaction { AppEvents(AppName("browser")).then(events)(HttpEvent.Outgoing(it)) })
+        .then(RequestTracing())
+        .then(PrintRequestAndResponse())
+        .then(client)
